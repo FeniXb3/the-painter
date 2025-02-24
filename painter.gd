33fe4a50424e9a99279cb5969569daebf731a24b
Konrad Gadzina <konrad@enklawa.edu.pl>
2025-02-24 17:12:07 +0100

    Add drawing on images

---
+++ painter.gd
@@ -0,0 +1,22 @@
+extends TextureRect
+
+@export var image_size := Vector2i(128, 128)
+@export_range(1, 10) var brush_size := 4
+var image: Image
+
+func _ready() -> void:
+	image = Image.create(image_size.x, image_size.y, false, Image.FORMAT_RGBA8)
+	image.fill(Color.WHITE)
+	texture = ImageTexture.create_from_image(image)
+
+
+func _process(delta: float) -> void:
+	var mouse_position: Vector2i = get_local_mouse_position()
+	if mouse_position.x < 0 or mouse_position.y < 0 or mouse_position.x >= image.get_size().x or mouse_position.y >= image.get_size().y:
+		return
+		
+	if Input.is_action_pressed("draw"):
+		for x in brush_size:
+			for y in brush_size:
+				image.set_pixelv(mouse_position + Vector2i(x-brush_size/2, y - brush_size/2), Color.BLACK)
+		texture = ImageTexture.create_from_image(image)
