6c306fcf5ee83e70de7a5cac6b1e350ac4bb8fba
Konrad Gadzina <konrad@enklawa.edu.pl>
2025-04-07 16:30:37 +0200

    Add changing brush size

--- painter.gd
+++ painter.gd
@@ -3,6 +3,7 @@ extends TextureRect
 
 @export var image_size := Vector2i(128, 128)
 @export_range(1, 10) var brush_size : float = 4
+@export var brush_increment_step: int = 2
 @export var background_color := Color.WHITE
 @export var brush_color := Color.BLACK
 @export var brush_texture: Texture2D:
@@ -11,11 +12,21 @@ extends TextureRect
 		brush_image = brush_texture.get_image()
 var image: Image
 var brush_image: Image
+var is_active: bool = false
 
 func _ready() -> void:
 	image = Image.create(image_size.x, image_size.y, false, Image.FORMAT_RGBA8)
 	image.fill(background_color)
 	texture = ImageTexture.create_from_image(image)
+	
+func _input(event: InputEvent) -> void:
+	if not is_active:
+		return
+
+	if event.is_action_pressed("increase_brush_size"):
+		brush_size += brush_increment_step
+	elif event.is_action_pressed("decrease_brush_size"):
+		brush_size -= brush_increment_step
 
 
 func _process(_delta: float) -> void:
@@ -57,3 +68,11 @@ func draw_brush(brush_position: Vector2, pixel_getter: Callable):
 			pixel_position = pixel_position.clamp(Vector2i(), image.get_size() - Vector2i(1, 1))
 			image.set_pixelv(pixel_position, brush_pixel)
 	texture = ImageTexture.create_from_image(image)
+
+
+func _on_mouse_entered() -> void:
+	is_active = true
+
+
+func _on_mouse_exited() -> void:
+	is_active = false
